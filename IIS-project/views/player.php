11f3d14d9e3a632e9a94c942b2958f513c6f4ea6
TomValent <tomas.valent7@gmail.com>
2022-11-26 11:18:28 +0100

    stats done

--- IIS-project/views/player.php
+++ IIS-project/views/player.php
@@ -12,12 +12,30 @@
     $q->execute(["id" => $_GET["id"]]);
     $user = $q->fetch(PDO::FETCH_NAMED);
 
-    $stmt = $pdo->prepare("SELECT * FROM Matches m left join Tournament t on m.TournamentID = t.TournamentID left join TournamentParticipant tp on t.TournamentID = tp.TournamentID where t.type = 'member' and tp.MemberID = :id");
+    $stmt = $pdo->prepare("SELECT * FROM Matches m left join Tournament t on m.TournamentID = t.TournamentID ".
+                                    "left join TournamentParticipant tp on t.TournamentID = tp.TournamentID ".
+                                    "where t.type = 'member' and tp.MemberID = :id");
     $stmt->execute(["id" => $_GET["id"]]);
     $stats = $stmt->fetchAll(PDO::FETCH_NAMED);
+
+    $q2 = $pdo->prepare("SELECT m.MatchID, m.Points1, m.Points2, t.Name, te.Name, te.TeamID FROM Matches m left join Tournament t on m.TournamentID = t.TournamentID ".
+                                    "left join TournamentParticipant tp on t.TournamentID = tp.TournamentID ".
+                                    "left join Team te on te.TeamID = tp.TeamID ".
+                                    "left join MemberTeam mt on mt.TeamID = te.TeamID ".
+                                    "where t.type = 'team' and mt.MemberTeamID = :id");
+    $q2->execute(["id" => $_GET["id"]]);
+    $statsT = $q2->fetchAll(PDO::FETCH_NAMED);
+//    var_dump($statsT);exit;
 ?>
 
-<span>Profile of <?php echo $user["Name"]; ?></span></br>
+<span>Profile of <?php echo $user["Name"]; ?></span></br><hr>
+<?php
+if (!empty($_GET["id"]) && $_SESSION["id"] === intval($_GET["id"])) {
+	$_GET["edit"] = "true";
+	echo "<div class='right'><button><a href='editAccount?id=". $_GET["id"] ."&edit=true'>Edit account</a></button></div>";
+	echo "<div class='right'><button onclick='deleteMember(" . $_GET["id"] . ")'>Delete account</button></div>";
+}
+?>
 <div class="center margin" style="margin-top:100px;">Matches stats</div>
 <table style="margin-top:5px;">
     <td>
@@ -63,6 +81,58 @@
     </tr>
     <?php } ?>
 </table>
+</br></br>
+<table style="margin-top:5px;">
+    <td>
+        Tournament
+    </td>
+    <td>
+        Team 1
+    </td>
+    <td>
+        Team 2
+    </td>
+    <td>
+        Score 1
+    </td>
+    <td>
+        Score 2
+    </td>
+    <?php foreach ($statsT as $row) {
+        $getID = $pdo->prepare("SELECT Team1ID, Team2ID FROM Matches WHERE MatchID = :id");
+		$getID->execute(["id" => $row["MatchID"]]);
+        $teamID = $getID->fetch(PDO::FETCH_NAMED);
+        $id2 = 0;
+
+        if ($teamID["Team1ID"] == $row["TeamID"]) {
+           $id2 =  $teamID["Team2ID"];
+        } else {
+            $id2 = $teamID["Team1ID"];
+        }
+
+		$getTeam2 = $pdo->prepare("SELECT Name FROM Team WHERE TeamID = :id");
+		$getTeam2->execute(["id" => $id2]);
+		$team2 = $getTeam2->fetch(PDO::FETCH_NAMED);
+    ?>
+        <tr>
+            <td>
+				<?php echo $row["Name"][0] ?>
+            </td>
+            <td>
+				<?php echo $row["Name"][1] ?>
+            </td>
+            <td>
+				<?php echo $team2["Name"] ?>
+            </td>
+            <td>
+				<?php echo $row["Points1"] ?>
+            </td>
+            <td>
+				<?php echo $row["Points2"] ?>
+            </td>
+        </tr>
+	<?php } ?>
+</table>
 <script>
     function deleteMember(id) {
         let data = {
@@ -74,10 +144,3 @@
         })
     }
 </script>
-<?php
-    if (!empty($_GET["id"]) && $_SESSION["id"] === intval($_GET["id"])) {
-		$_GET["edit"] = "true";
-        echo "<div class='right'><button><a href='editAccount?id=". $_GET["id"] ."&edit=true'>Edit account</a></button></div>";
-        echo "<div class='right'><button onclick='deleteMember(" . $_GET["id"] . ")'>Delete account</button></div>";
-    }
-?>
